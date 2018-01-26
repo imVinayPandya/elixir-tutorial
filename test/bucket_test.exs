1bfc5fcc25f6bdcc6458867da8a06c08564aa998
Vinay Pandya <005vinay@gmail.com>
2018-01-26 16:38:27 +0530

    Elixir GenServer example

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,27 +1,27 @@
 defmodule KV.BucketTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    setup do
-        {:ok, bucket} = KV.Bucket.start_link([])
-        %{bucket: bucket}
-    end
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
 
-    test "store values by keys", %{bucket: bucket} do
-        assert KV.Bucket.get(bucket, "milk") == nil
+  test "store values by keys", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
 
-        KV.Bucket.put(bucket, "milk", 3)
-        assert KV.Bucket.get(bucket, "milk") == 3
-    end
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
 
-    test "delete key from bucket", %{bucket: bucket} do
-        assert KV.Bucket.get(bucket, "milk") == nil
+  test "delete key from bucket", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
 
-        KV.Bucket.put(bucket, "milk", 3)
-        assert KV.Bucket.get(bucket, "milk") == 3
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
 
-        KV.Bucket.delete(bucket, "milk")
-        assert KV.Bucket.get(bucket, "milk") == nil
-    end
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
 
 # defmodule KV.BucketTest do
